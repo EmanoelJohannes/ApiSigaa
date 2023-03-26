7e79520a74b755bbd6fc2f0d5db488bf4bb4ef7d
EmanoelJohannes <emanoeljohannes@hotmail.com>
2023-03-26 18:47:20 -0300

    Update

--- src/app/controllers/DocentesController.js
+++ src/app/controllers/DocentesController.js
@@ -16,32 +16,40 @@ class UserController {
         }
         try {
           const docentesData = JSON.parse(data);
-
+          
           // Processa os dados utilizando o método reduce
-          const result = docentesData.reduce((acc, docente) => {
+          const result = docentesData.reduce((acc, current) => {
             const departamentos = acc.departamentos;
             let totalManager = acc.totalManager;
             const managerFromDepartaments = acc.managerFromDepartaments;
-            docente.docentes.forEach((element) => {
-              // Popula o array de departamentos              
-              if(departamentos.indexOf(element.Departamento) === -1) {
-                departamentos.push(element.Departamento);
-              }
-
-              // Calcula o total de docentes por departamento
-              const departamentoIndex = managerFromDepartaments.findIndex((d) => d[0] === element.Departamento);
-              if(departamentoIndex === -1) {
-                managerFromDepartaments.push([element.Departamento, 1]);
-              } else {
-                managerFromDepartaments[departamentoIndex][1] += 1;
-              }
+            const years = new Set(acc.years);
+          
+            // Adiciona o ano à lista de anos encontrados
+            current.forEach((projeto) => {
+              years.add(parseInt(projeto.codigo.split('-')[1]));
+          
+              projeto.docentes.forEach((docente) => {
+                // Popula o array de departamentos
+                if (departamentos.indexOf(docente.Departamento) === -1) {
+                  departamentos.push(docente.Departamento);
+                }
+          
+                // Calcula o total de docentes por departamento
+                const departamentoIndex = managerFromDepartaments.findIndex((d) => d[0] === docente.Departamento);
+                if (departamentoIndex === -1) {
+                  managerFromDepartaments.push([docente.Departamento, 1]);
+                } else {
+                  managerFromDepartaments[departamentoIndex][1] += 1;
+                }
+              });
+          
+              // Calcula o total de docentes do projeto
+              totalManager += projeto.qntd_docentes;
             });
-            
-            // Calcula o total de docentes
-            totalManager += docente.docentes.length;
-            return { departamentos, totalManager, managerFromDepartaments };
-          }, { departamentos: [], totalManager: 0, managerFromDepartaments: [] });
-  
+          
+            return { departamentos, totalManager, managerFromDepartaments, years: [...years] };
+          }, { departamentos: [], totalManager: 0, managerFromDepartaments: [], years: [] });
+          
           result.managerFromDepartaments.unshift(['Departamento', 'Quantidade']);
 
           callback(null, result);
@@ -72,8 +80,10 @@ class UserController {
         try {
           const docentesData = JSON.parse(data);
 
+          const filteredData = docentesData.filter(data => data[0].codigo.includes(`-${year}`));
+
           // Processa os dados utilizando o método reduce
-          const result = docentesData.reduce((acc, docente) => {
+          const result = filteredData[0].reduce((acc, docente) => {
             const departamentos = acc.departamentos;
             let totalManager = acc.totalManager;
             const managerFromDepartaments = acc.managerFromDepartaments;
--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const authMiddleware = require('./app/middlewares/auth');
 const docentesController = require('./app/controllers/DocentesController');
 
 // Rotas a partir desse ponto são rotas com autentificação
-routes.get('/docentes/:year', docentesController.getDocentes);
+routes.get('/docentes', docentesController.getDocentes);
 routes.get('/docentes-by-year/:year', docentesController.getDocentesByYear);
 
 routes.use(authMiddleware);
